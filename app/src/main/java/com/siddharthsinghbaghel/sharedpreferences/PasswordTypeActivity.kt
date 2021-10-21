8e294c1d47bd682f44bf18438f9f103d21961abe
Siddharth Singh Baghel <siddharthsinghrewa5@gmail.com>
2021-10-21 15:39:13 +0530

    Password type activity

--- app/src/main/java/com/siddharthsinghbaghel/sharedpreferences/PasswordTypeActivity.kt
+++ app/src/main/java/com/siddharthsinghbaghel/sharedpreferences/PasswordTypeActivity.kt
@@ -2,6 +2,8 @@ package com.siddharthsinghbaghel.sharedpreferences
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class PasswordTypeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,6 +11,17 @@ class PasswordTypeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_password_type)
 
 
+        val btn0: Button = findViewById(R.id.btn0)
+        val btn1:Button = findViewById(R.id.btn1)
+        val btn2:Button = findViewById(R.id.btn2)
+        val btn3:Button = findViewById(R.id.btn3)
+        val btn4:Button = findViewById(R.id.btn4)
+        val btn5:Button = findViewById(R.id.btn5)
+        val btn6:Button = findViewById(R.id.btn6)
+        val btn7:Button = findViewById(R.id.btn7)
+        val btn8:Button = findViewById(R.id.btn8)
+        val btn9:Button = findViewById(R.id.btn9)
+        val btnErase: ImageButton = findViewById(R.id.btnErase)
 
 
 
